0c46396a93992697c30d0b5b733cf37df1ff5482
jumi525 <jumiklein525@gmail.com>
2024-11-01 15:30:53 +0100

    deployment issue again

--- src/components/global/locals.tsx
+++ src/components/global/locals.tsx
@@ -1,18 +1,34 @@
-"use client";
-import { Profile } from "@/lib/types";
-import { useEffect, useState } from "react";
+// "use client";
+// import { Profile } from "@/lib/types";
 
-export const useLocalStorage = (key: string) => {
-  const [value, setValue] = useState<Profile>(() =>
-    JSON.parse(window.localStorage.getItem(key) || "")
-  );
+// import { useState, useEffect } from "react";
 
-  useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(value));
-  }, [value, key]);
-  return [value, setValue] as const;
-};
-export default useLocalStorage;
+// export default function useLocalstorage(key: string) {
+//   const [value, setValue] = useState<Profile | null>(null);
+//   useEffect(() => {
+//     const [value, setValue] = useState<Profile>(() =>
+//       JSON.parse(window.localStorage.getItem(key) || "")
+//     );
+//   }, []);
+//   useEffect(() => {
+//     setValue(window.localStorage.setItem(key, JSON.stringify(value)) | null)
+//   }, [value, key]);
+//   return [value, setValue];
+// }
+
+// import { useEffect, useState } from "react";
+
+// export const useLocalStorage = (key: string) => {
+//   const [value, setValue] = useState<Profile>(() =>
+//     JSON.parse(window.localStorage.getItem(key) || "")
+//   );
+
+// useEffect(() => {
+//   window.localStorage.setItem(key, JSON.stringify(value));
+// }, [value, key]);
+//   return [value, setValue] as const;
+// };
+// export default useLocalStorage;
 
 // const updatedData = { ...user, ...userProfile };
 // useEffect(() => {
